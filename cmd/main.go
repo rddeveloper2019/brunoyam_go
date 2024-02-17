52ef593d7cc89304076183e2e962f935123dcb09
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2024-02-17 10:06:26 +0300

    hw-2

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+const ADULT = 18
+
+func isAdult(age int) bool {
+	return age >= ADULT
+}
+
+func checkForAdults(ages []int) string {
+	for _, age := range ages {
+		if isAdult(age) {
+			return "Yes"
+		}
+	}
+	return "No"
+}
+
+func findUnderageNames(persons map[string]int) (adults []string, err error) {
+	for name, age := range persons {
+		if !isAdult(age) {
+			adults = append(adults, name)
+		}
+	}
+	if len(adults) > 0 {
+		err = errors.New("Underages found!")
+	}
+	return
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	alex := 18
+	maxim := 10
+	maria := 7
+
+	//Task 1
+
+	fmt.Println("Alex is adult:", isAdult(alex))
+	fmt.Println("Maxim is adult:", isAdult(maxim))
+	fmt.Println("Maria is adult:", isAdult(maria))
+
+	//Task 2
+
+	fmt.Println(checkForAdults([]int{alex, maria, maxim}))
+	fmt.Println(checkForAdults([]int{maxim}))
+	fmt.Println(checkForAdults([]int{}))
+
+	//Task 3
+	persons := map[string]int{
+		"alex":   14,
+		"maxim":  18,
+		"maria":  10,
+		"georgy": 79,
+		"mike":   0,
+	}
+
+	if adults, err := findUnderageNames(persons); err != nil {
+		fmt.Println(err)
+		fmt.Println(adults)
+	}
+	if adults, err := findUnderageNames(map[string]int{}); err != nil {
+		fmt.Println(err)
+		fmt.Println(adults)
+	}
 }
